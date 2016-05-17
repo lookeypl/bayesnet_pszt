721f25cd32cf415fa7271405ec7143db0fd812b8
Lukasz Kostyra <costyrra.xl@gmail.com>
2016-05-17 09:41:53 +0200

    Bayes: Introduce Probability Matrix
    The commit expands BayesNode module with a Probability Matrix. It
    will be used to calculate attribute values for nodes.
    
    Verification:
        Create some nodes and click on them. On console there should be
        their probability matrix printed:
          * Nodes without parents will have same probabilities as attributes
          * Nodes with parents will have undefined probabilities, and the
            matrix will have AttrCount x CombCount dimension, where
              - AttrCount is an attribute count for current node
              - CombCount is calculated by multiplying parents'
                attribute counts
    
    Change-Id: I284ae12e1e903cb4a56581e79007307fab7880c2

--- src/bayesnet_pszt/BayesNode.java
+++ src/bayesnet_pszt/BayesNode.java
@@ -4,13 +4,19 @@ import java.util.Vector;
 
 public class BayesNode {
     /** Our probabilities to recalculate for each node */
-    public Vector<BayesAttributePair<Float>> mAttrs;
+    public Vector<BayesAttributePair<Float>> mAttrs = new Vector<BayesAttributePair<Float>>();
+
+    /** Probability matrix, used for calculations */
+    public Vector<Float> mProbMatrix = new Vector<Float>();
+    private int mAttributeCount = 0;
+    private int mCombinationCount = 0;
+    private final float UNFILLED_FIELD = -1.0f;
 
     /** Name of the node, for clarity sake */
     public String mName;
 
     /** True if we are an evidence node, false if we are not. */
-    private boolean mIsEvidence;
+    private boolean mIsEvidence = false;
 
     /** Parents of given node, to preserve tree structure */
     Vector<BayesNode> mParents = new Vector<BayesNode>();
@@ -19,30 +25,74 @@ public class BayesNode {
     Vector<BayesNode> mChildren = new Vector<BayesNode>();
 
     public BayesNode(String name) {
-        mIsEvidence = false;
         mName = name;
+    }
 
-        // temporarily set attributes, just to display something
-        mAttrs = new Vector<BayesAttributePair<Float>>();
-        BayesAttributePair<Float> temp = new BayesAttributePair<Float>();
-        temp.key = "A";
-        temp.value = 0.28f;
-        mAttrs.add(temp);
-        temp = new BayesAttributePair<Float>();
-        temp.key = "B";
-        temp.value = 0.06f;
-        mAttrs.add(temp);
-        temp = new BayesAttributePair<Float>();
-        temp.key = "0";
-        temp.value = 0.66f;
-        mAttrs.add(temp);
+    public void SetAttrs(Vector<BayesAttributePair<Float>> attrs)
+    {
+        if (attrs == null)
+            throw new IllegalArgumentException();
+
+        // replace previous attribute array if new is provided
+        mAttrs = attrs;
+        InitializeProbMatrix();
+    }
+
+    public void InitializeProbMatrix()
+    {
+        // generate a probability matrix from the data we curretly have
+        // Attribute count is easy - extract from mAttrs
+        // Combination count depends on parents - it is all combinations
+        // between all variables possible.
+        mAttributeCount = mAttrs.size();
+        mCombinationCount = 1;
+        if (!mParents.isEmpty())
+            for (BayesNode parent : mParents)
+                mCombinationCount *= parent.GetAttributeCount();
+
+        // drop previous attribute matrix and generate a new undefined one
+        mProbMatrix = new Vector<Float>();
+        for (int i = 0; i < mAttributeCount * mCombinationCount; ++i)
+        {
+            if (mParents.isEmpty())
+                // we have no parents :( So we can take the probs from attrs
+                mProbMatrix.add(mAttrs.get(i).value);
+            else
+                // we have parents, yay! The matrix will be manually set
+                mProbMatrix.add(UNFILLED_FIELD);
+        }
+    }
+
+    public void SetProbability(int combination, int attribute, float value)
+    {
+        if (combination > mCombinationCount)
+            throw new ArrayIndexOutOfBoundsException();
+
+        if (attribute > mAttributeCount)
+            throw new ArrayIndexOutOfBoundsException();
+
+        mProbMatrix.set(combination * mAttributeCount + attribute, value);
+    }
+
+    public void SetProbability(Vector<Float> probMatrix)
+    {
+        if (probMatrix == null)
+            throw new IllegalArgumentException();
+
+        // probability matrix has size depending on attributes and parents
+        // ensure this is a good matrix we receive here
+        if (probMatrix.size() != mProbMatrix.size())
+            throw new IllegalArgumentException();
+
+        mProbMatrix = probMatrix;
     }
 
     public void SetEvidence(boolean isEvidence, Vector<BayesAttributePair<Float>> evidenceAttrs) {
         mIsEvidence = isEvidence;
+        mAttrs = evidenceAttrs;
 
         if (mIsEvidence) {
-            mAttrs = evidenceAttrs;
+            // TODO: recalc probabilities here
         }
     }
 
@@ -52,67 +102,88 @@ public class BayesNode {
 
     public void AddParent(BayesNode parent) {
         if (parent == null)
-            return;
+            throw new IllegalArgumentException();
 
-        if (parent == this)
-            return;
-
-        if (mParents.contains(parent))
-            return;
-
-        if (mChildren.contains(parent))
+        if (parent == this || mParents.contains(parent) || mChildren.contains(parent))
             return;
 
         mParents.add(parent);
         parent.AddChild(this);
+        InitializeProbMatrix(); // TODO this leads to double-updating the matrix.
     }
 
     public void AddChild(BayesNode child) {
         if (child == null)
-            return;
+            throw new IllegalArgumentException();
 
-        if (child == this)
-            return;
-
-        if (mChildren.contains(child))
-            return;
-
-        if (mParents.contains(child))
+        if (child == this || mChildren.contains(child) || mParents.contains(child))
             return;
 
         mChildren.add(child);
         child.AddParent(this);
+        InitializeProbMatrix();
     }
 
     public void RemoveParent(BayesNode parent) {
         if (parent == null)
-            return;
+            throw new IllegalArgumentException();
 
-        if (parent == this)
-            return;
-
-        if (!mParents.contains(parent))
+        if (parent == this || !mParents.contains(parent))
             return;
 
         mParents.remove(parent);
         parent.RemoveChild(this);
+        InitializeProbMatrix();
     }
 
     public void RemoveChild(BayesNode child) {
         if (child == null)
-            return;
+            throw new IllegalArgumentException();
 
-        if (child == this)
-            return;
-
-        if (!mChildren.contains(child))
+        if (child == this || !mChildren.contains(child))
             return;
 
         mParents.remove(child);
         child.RemoveParent(this);
+        InitializeProbMatrix();
     }
 
-    public int GetParamCount() {
-        return mAttrs.size();
+    public void RemoveAllChildren() {
+        for (BayesNode child : mChildren)
+            RemoveChild(child);
+
+        mChildren.clear();
+    }
+
+    public void RemoveAllParents() {
+        for (BayesNode parent : mParents)
+            RemoveParent(parent);
+
+        mParents.clear();
+    }
+
+    public int GetAttributeCount() {
+        if (mAttrs != null)
+            return mAttrs.size();
+        else
+            return 0;
+    }
+
+    // for debugging purposes
+    public void PrintProbsToStdout() {
+        System.out.println("Node " + mName + " probability matrix:");
+        int attributeCounter = 0;
+        for (Float prob : mProbMatrix)
+        {
+            if (attributeCounter == mAttributeCount)
+            {
+                attributeCounter = 0;
+                System.out.print("\n");
+            }
+            System.out.print(prob.toString() + " ");
+            attributeCounter++;
+        }
+
+        System.out.println("\n");
     }
 }
--- src/bayesnet_pszt/GuiElement.java
+++ src/bayesnet_pszt/GuiElement.java
@@ -2,4 +2,5 @@ package bayesnet_pszt;
 
 public interface GuiElement {
     public boolean isLocationHit(int x, int y);
+    public void onDestroy();
 }
